7d82138ed9820a01cac1844fc22e2fd91ae857c8
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-05-15 21:57:20 -0500

    Update sintesis.vue

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="La síntesis presenta las pruebas de software que aseguran la calidad ISO/IEC evaluando aspectos funcionales y no funcionales, incluyendo pruebas unitarias, de integración y de rendimiento. Estas pruebas permiten verificar el comportamiento individual de los módulos, la interacción entre componentes y la eficiencia bajo condiciones de trabajo específicas. Su implementación es clave para identificar errores, optimizar recursos y garantizar la confiabilidad y el rendimiento esperado del producto final.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
